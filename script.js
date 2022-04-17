667bd6a8c9625a783634082e7a5c285f3c0257f1
Arif Khan <mdshafiqul0404@gmail.com>
2022-04-17 12:53:36 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const AllBtn = document.querySelectorAll('.btn');
+const display = document.querySelector(".display")
+const AC = document.querySelector('.clear')
+const Del = document.querySelector('.del')
+const equal = document.querySelector('.equal')
+console.log(AllBtn)
+
+AllBtn.forEach(button=> {
+    button.addEventListener('click', ()=> {
+        if(display.innerHTML == 0) {
+            display.innerHTML = ""
+        }
+        
+        display.innerHTML += button.innerHTML
+    })
+})
+
+AC.addEventListener('click', ()=> {
+    display.innerHTML = '0'
+})
+
+Del.addEventListener('click', ()=> {
+    display.innerHTML = display.innerHTML.slice(0, -1);
+    if(display.innerHTML == '') {
+        display.innerHTML = '0'
+    }
+})
+
+equal.addEventListener('click', ()=> {
+    display.innerHTML = eval(display.innerHTML)
+})
